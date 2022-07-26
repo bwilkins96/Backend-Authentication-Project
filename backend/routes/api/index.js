bd83bca228b3267962c7646071f9c11a467921b9
Ben <ben.jr.speed@gmail.com>
2022-07-25 20:34:46 -0400

    phase5!

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -2,9 +2,6 @@ const router = require('express').Router();
 const sessionRouter = require('./session');
 const usersRouter = require('./users');
 
-//const { setTokenCookie, restoreUser, requireAuth } = require('../../utils/auth.js');
-//const { User } = require('../../db/models');
-
 router.use('/session', sessionRouter);
 router.use('/users', usersRouter);
 
@@ -14,27 +11,3 @@ router.post('/test', (req, res) => {
 
 
 module.exports = router;
-
-
-
-//testing routes
-/*
-router.get('/set-token-cookie', async (req, res) => {
-    const user = await User.findOne({
-        where: {
-            username: 'Demo-lition'
-        }
-    });
-
-    setTokenCookie(res, user);
-    return res.json({ user });
-});
-
-router.get('/restore-user', restoreUser, (req, res) => {
-    return res.json(req.user);
-});
-
-router.get('/require-auth', requireAuth, (req, res) => {
-    return res.json(req.user);
-});
-*/
